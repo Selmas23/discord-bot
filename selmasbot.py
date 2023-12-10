7cb1cf2d87f185b0c9cc9658bf7ac455c6b92d61
Selmas23 <torresselma@outlook.com>
2023-12-09 20:59:04 -0800

    added comments

--- selmasbot.py
+++ selmasbot.py
@@ -6,6 +6,7 @@ import random
 from ec2_metadata import ec2_metadata
 from dotenv import load_dotenv
 
+#Loading the env file
 load_dotenv()
 
 print(ec2_metadata.region)
@@ -31,11 +32,16 @@ async def on_message(message):
     channel = str(message.channel.name) 
     user_message = str(message.content) 
 
+#The bot should read the message, the user name and the channel
     print(f'Message {user_message} by {username} on {channel}') 
-#if
+
     if message.author == client.user: 
         return
-
+#Messages should be in the "random" channel
+#If the message is in the random channel than the bot will respond to hello or hi
+#The bot should respond to hello or hi with Hello and the username
+#If user responds bye the bot should respond with Bye
+#If user asks bot, "motivate me" bot should respond with one of the four motivational outputs, this will be at random
     if channel == "random": 
         if user_message.lower() == "hello" or user_message.lower() == "hi": 
             await message.channel.send(f'Hello {username}') 
@@ -48,10 +54,11 @@ async def on_message(message):
                     "No pressure, No diamonds.", 
                     "The only thing that overcomes hard luck is hard work."] 
             await message.channel.send(random.choice(motivation)) 
-
+#If user sends hello world bot responds "hello"
         elif user_message.lower() == 'hello world':
             await message.channel.send('hello')
-
+#IF user says tell me about your server the bot will respond with server info
+#If the user says anything that is not any of the things listed above it will respond with a message saying the command is invalid
         elif user_message.lower() == ("tell me about your server"):
             await message.channel.send(f"""your ec2 server data:\n
 region:{ec2_metadata.region}\n
@@ -61,5 +68,5 @@ server instance:{ec2_metadata.instance_type}""")
         else:
             await message.channel.send(f"I'm sorry, the command '{user_message}' is not a valid command")
 
-#Bot begins running
+#Bot begins running YAY!
 client.run(token)
